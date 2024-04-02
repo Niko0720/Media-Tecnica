1e2dab64212d62ed248990d50f36a77ab06f0e35
Niko0720 <165803572+Niko0720@users.noreply.github.com>
2024-04-02 10:54:33 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import {pinturas} from "./DATA/data.js"
+
+console.log(pinturas)
+
+const items = document.getElementById('Items');
+const templatecard = document.getElementById('Template-card').content;
+const fragment = document.createDocumentFragment();
+
